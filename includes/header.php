1db872dab6a072cf78777d3b3e7d888d925000df
jericovendivel <jericovendivel122@gmail.com>
2025-12-04 16:53:25 +0800

    Add header.php with basic HTML structure

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sugar Shack</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>Sugar Shack</h1>
+    <nav>
+      <a href="index.php">Home</a> | 
+      <a href="product.php">Products</a> | 
+      <a href="about.php">About</a> | 
+      <a href="contact.php">Contact</a>
+    </nav>
